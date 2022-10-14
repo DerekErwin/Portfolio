84cf8ae7ae269ac3d82154eaacc8d1c034a48cfb
Derek Erwin <21228217+DerekErwin@users.noreply.github.com>
2022-10-13 20:59:47 -0400

    Complete remake of the site in BootStrap

---
+++ javascript.js
@@ -0,0 +1,10 @@
+/**Preloader*/
+document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+    const preloader = document.querySelector('#preloader');
+    if (preloader) {
+      window.addEventListener('load', () => {
+        preloader.remove();
+      });
+    }
+  });
